1db91388270261db74e0f09c086cb9b99026cbbf
Prsalunk <salunkeprati94@gmail.com>
2020-06-03 18:32:27 +0530

    6.add-fetch data from db, by created user by admin

--- pract_django/app1/views.py
+++ pract_django/app1/views.py
@@ -4,27 +4,6 @@ from .models import Destination
 
 def index(request):
 
-    dest1 = Destination()
-    dest1.name = 'pune'
-    dest1.price = 2000
-    dest1.desc = 'Sooo many experiences'
-    dest1.image = 'destination_1.jpg'
-    dest1.offr = False
-
-    dest2 = Destination()
-    dest2.name = 'mumbai'
-    dest2.price = 1000
-    dest2.desc = 'Cheap logonka residences'
-    dest2.image = 'destination_2.jpg'
-    dest2.offr = True
-
-    dest3 = Destination()
-    dest3.name = 'hyderabad'
-    dest3.price = 3000
-    dest3.desc = 'Wanna go for Biryani'
-    dest3.image = 'destination_3.jpg'
-    dest3.offr = False
-
-    dests = [dest1,dest2,dest3]
+    dests = Destination.objects.all()
 
     return render(request, 'index.html', {'dests':dests})
