41059975bef4c1432591df6e58299757a51a2097
Prsalunk <salunkeprati94@gmail.com>
2020-06-05 00:30:48 +0530

    9.Login-Logout

--- pract_django/accounts/views.py
+++ pract_django/accounts/views.py
@@ -33,3 +33,25 @@ def register(request):
 
     else:
         return render(request, 'register.html')
+
+
+def login(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,'Invalid Authentication')
+            return redirect('login')
+
+    else:      
+        return render(request,'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return render(request,'logout.html')
